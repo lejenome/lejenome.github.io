a583733064c7b2b44c0e20f257ab75069470288c
Moez Bouhlel <bmoez.j@gmail.com>
2016-09-12 00:17:52 +0100

    replace syteup web blog with new one

--- js/blog.js
+++ js/blog.js
@@ -1,71 +1,78 @@
-function setupBlog(settings) {
-    "use strict";
-    var postOffset, postsOpts;
-    $(document).on("click", ".post-link", function () {
-        if (this.dataset["id"]) {
-            postsOpts = { id: this.dataset["id"] };
-            fetchBlogPosts(0, settings["blogs_settings"][settings["blog_platform"]], settings["blog_platform"], postsOpts);
-        }
-        postOffset = 0;
-    });
-    $(document).on("click", ".tag-link", function () {
-        if (this.textContent) {
-            window.reachedEnd = false;
-            postsOpts = { tag: this.textContent };
-            fetchBlogPosts(0, settings["blogs_settings"][settings["blog_platform"]], settings["blog_platform"], postsOpts).then(function (offset) {
-                postOffset = offset;
-            });
-        }
-    });
-    $("#home-link").on("click", function () {
-        if (location.hash.substr(0, 2) !== "#!")
-            return;
-        location.hash = "";
-        postsOpts = undefined;
-        window.reachedEnd = false;
-        fetchBlogPosts(0, settings["blogs_settings"][settings["blog_platform"]], settings["blog_platform"]).then(function (offset) {
-            postOffset = offset;
-        });
-    });
-    //SETUP LINKS & BLOG
-    window.sharethis_enabled = settings["blogs_settings"]["plugins"]["sharethis"] || false;
-    if (settings["blogs_settings"]["plugins"]["disqus"])
-        window.disqusPlugin.setup(settings["plugins_settings"]["disqus"]);
-    window.reachedEnd = false;
-    // set to true if no more blog posts left.
-    if (location.hash.substr(0, 7) === "#!post/")
-        postsOpts = { id: location.hash.slice(7).split("#")[0] };
-    else if (location.hash.substr(0, 6) === "#!tag/")
-        postsOpts = { tag: location.hash.slice(6).split("#")[0] };
-    return fetchBlogPosts(0, settings["blogs_settings"][settings["blog_platform"]], settings["blog_platform"], postsOpts).then(function (offset) {
-        postOffset = offset;
-        var resultsLoaded = false, scrollWait = false, scrollWaitDur = 250;
-        $(window).scroll(function () {
-            if (!window.reachedEnd && !resultsLoaded && !scrollWait && postOffset && $(window).scrollTop() + $(window).height() > $(document).height() / 1.2) {
-                resultsLoaded = true;
-                scrollWait = true;
-                fetchBlogPosts(postOffset, settings["blogs_settings"][settings["blog_platform"]], settings["blog_platform"], postsOpts).then(function (offset) {
-                    postOffset = offset;
-                    scrollWait = false;
-                });
-                // Only load posts at most every scrollWaitDur milliseconds.
-                setTimeout(function () {
-                    scrollWait = false;
-                }, scrollWaitDur);
-            }
-            if (resultsLoaded && $(window).scrollTop() + $(window).height() < $(document).height() / 1.2) {
-                resultsLoaded = false;
-            }
-        });
-        if (window.sharethis_enabled) {
-            var switchTo5x = true;
-            var jsfile = document.createElement("script");
-            jsfile.src = "http://w.sharethis.com/button/buttons.js";
-            jsfile.type = "text/javascript";
-            //jsfile.async = true;
-            document.body.appendChild(jsfile);
-            stLight.options({ publisher: settings["plugins_settings"]["sharethis"]["publisher_key"] });
-        }
-        return Promise.resolve();
-    });
+/* global getPosts, prettyPrint, POSTS_MAX */
+"use strict";
+function post2html(post)
+{
+	let txt = `<article id="${post.id}">
+                 <hgroup>
+                   <h2><a data-id="${post.id}" href="#!post/${post.id}" class="post-link">${"post.title"}</a></h2>
+                   <h3 class="post-date"><a href="#${post.id}">${(new Date(post.date)).toDateString()}</a></h3>
+                 </hgroup>
+                 <div class="post-body">
+                   ${post.body}
+                 </div>
+               </article>`;
+	let oParser = new DOMParser();
+	let oDOM = oParser.parseFromString(txt, "text/html");
+	let el = oDOM.documentElement.children[1]
+		     .children[0]; // root -> body -> articale
+	el.getElementsByClassName("post-link")[0].textContent = post.title;
+	return el;
 }
+function loadPosts()
+{
+	let postId = 0;
+	let pageId = 0;
+	let container = document.getElementById("articales");
+	let nextBtn = document.getElementById("next-post");
+	let prevBtn = document.getElementById("prev-post");
+	if (location.hash.startsWith("#!")) {
+		let [type, id] = location.hash.slice(2).split("/");
+		try {
+			switch (type) {
+			case "page":
+				pageId = parseInt(id);
+				break;
+			case "post":
+				postId = parseInt(id);
+				break;
+			}
+		} catch (e) {
+		}
+	}
+	getPosts({postId, pageId})
+	    .then((posts) => {
+		    while (container.children.length)
+			    container.children[0].remove();
+		    posts.forEach(post =>
+				      container.appendChild(post2html(post)));
+		    prettyPrint();
+		    if (posts.length === 0)
+			    container.innerHTML = "<p>Oops! 404 Error!</p>";
+		    if (pageId && pageId > 1) {
+			    prevBtn.style.display = "inline";
+			    prevBtn.href =
+				location.toString().replace(location.hash, "");
+			    prevBtn.href += "#!page/" + (pageId - 1);
+			    prevBtn.onclick = (e) => window.location =
+				e.target.href;
+		    }
+		    if (posts.length === POSTS_MAX) {
+			    nextBtn.style.display = "inline";
+			    nextBtn.href =
+				location.toString().replace(location.hash, "");
+			    nextBtn.href += "#!page/" + ((pageId || 1) + 1);
+			    nextBtn.onclick = (e) => window.location =
+				e.target.href;
+		    }
+	    })
+	    .catch((err) => {
+		    container.innerHTML = "<p>Oops! Error loading posts!</p>";
+		    console.error(err);
+	    });
+}
+function main()
+{
+	window.addEventListener("hashchange", (e) => location.reload());
+	loadPosts();
+}
+main();
