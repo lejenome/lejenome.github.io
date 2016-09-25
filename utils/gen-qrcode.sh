d97c9c335b1aa99a4e596f97e32d20267bd3d836
Moez Bouhlel <bmoez.j@gmail.com>
2016-09-25 21:22:35 +0100

    update gen-qrcode

--- utils/gen-qrcode.sh
+++ utils/gen-qrcode.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 INPUT=${1:-vcard.min.vcf}
 OUTPUT=${2:-qrcode.png}
-cat "$INPUT" | qrencode -o "$OUTPUT" -s 20 -m 0 -d 300 --background=00000000 --foreground=000000ff --verbose
+FG=${3:-000000}
+cat "$INPUT" | qrencode -o "$OUTPUT" -s 20 -m 0 -d 300 --background=00000000 --foreground=${FG}ff --verbose
