69c02389d511129e3485deaea88a90b97c4bfcac
Moez Bouhlel <bmoez.j@gmail.com>
2016-09-27 15:56:15 +0100

    update card size and color

--- utils/gen-qrcode.sh
+++ utils/gen-qrcode.sh
@@ -2,4 +2,4 @@
 INPUT=${1:-vcard.min.vcf}
 OUTPUT=${2:-qrcode.png}
 FG=${3:-000000}
-cat "$INPUT" | qrencode -o "$OUTPUT" -s 20 -m 0 -d 300 --background=00000000 --foreground=${FG}ff --verbose
+cat "$INPUT" | qrencode -o "$OUTPUT" -s 40 -m 0 -d 600 --background=00000000 --foreground=${FG}ff --verbose
